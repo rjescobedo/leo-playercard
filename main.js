f1b5a653c07283206cac19fef263f8a3513f37cc
Raul Escobedo <rjescobedo@gmail.com>
2024-12-22 07:58:37 -0600

    size of card, added opacity, made it look better

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const leo = [
     {
         name: 'Leo Escobedo',
         position: 'Midfielder',
-        rating: '99',
         number: '27',
         age: '10',
         club: 'HTX',
         pass: '95',
         shot: '12',
-        speed: '7'
+        speed: '8'
     }
 ]
 
@@ -18,7 +17,7 @@ leoName.innerHTML = leo[0].name;
 
 //Position
 const leoPosition = document.getElementById('position');
-leoPosition.innerHTML = `Position: ${leo[0].position}`;
+leoPosition.innerHTML = leo[0].position;
 
 //Number
 const leoNumber = document.getElementById('number');
@@ -26,24 +25,20 @@ leoNumber.innerHTML = `#${leo[0].number}`;
 
 //Age
 const leoAge = document.getElementById('age');
-leoAge.innerHTML = `Age: ${leo[0].age}`;
+leoAge.innerHTML = `AGE: ${leo[0].age}`;
 
 //Club
 const leoClub = document.getElementById('club');
-leoClub.innerHTML = `Club: ${leo[0].club}`;
+leoClub.innerHTML = `CLB: ${leo[0].club}`;
 
 //Pass
 const leoPass = document.getElementById('pass');
-leoPass.innerHTML = `Passes: ${leo[0].pass}`;
+leoPass.innerHTML = `PAS: ${leo[0].pass}`;
 
 //Shot
 const leoShot = document.getElementById('shot');
-leoShot.innerHTML = `Shots: ${leo[0].shot}`;
+leoShot.innerHTML = `SHO: ${leo[0].shot}`;
 
 //Speed
 const leoSpeed = document.getElementById('speed');
-leoSpeed.innerHTML = `Speed: ${leo[0].speed} mph`;
-
-//Rating
-const leoRating = document.getElementById('rating');
-leoRating.innerHTML = leo[0].rating;
+leoSpeed.innerHTML = `SPD: ${leo[0].speed} mph`;
